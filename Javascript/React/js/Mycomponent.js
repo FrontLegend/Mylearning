c83aa00f9807de53ebcc273f80c69e8378df1ef9
FrontLegend <156782276@qq.com>
2021-04-02 20:21:25 +0800

    vim set paste

--- Javascript/React/js/Mycomponent.js
+++ Javascript/React/js/Mycomponent.js
@@ -1,26 +1,98 @@
+
 class Toggle extends React.Component {
-	
-	constructor (props) {
-		super (props);
-		
-		this.state = {
-			this.isToggleOn : true
-		}
-		this.handleToggle = this.handleToggle.bind(this)
-	}
-
-	handleToggle () {
-		this.setState(preState => 			({
-			isToggleOn : !preState.isToggleOn
-		})
-	}
-
-	render () {
-		return <button onClick={this.handleToggle}>{isToggleOn ? "ON" : "OFF"}</button>
-	}
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+ 
+    // 这边绑定是必要的，这样 `this` 才能在回调函数中使用
+    this.handleClick = this.handleClick.bind(this);
+  }
+ 
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+ 
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
 }
+ 
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('app')
+);
 
-const eName = "app"
-let tDom = document.getElementById(eName)
 
-ReactDOM.render(<Toggle/>, tDom)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
