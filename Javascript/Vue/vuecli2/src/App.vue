f4ddf6560b0de8a4ea8730099d748cc1fa45699c
FrontLegend <156782276@qq.com>
2021-04-07 13:08:59 +0800

    local

--- Javascript/Vue/vuecli2/src/App.vue
+++ Javascript/Vue/vuecli2/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
-    <chat-com></chat-com>
-    <userlist-com :userlist="userlist"></userlist-com>
+    <div class="content">
+      <chat-com></chat-com>
+      <userlist-com :userlist="userlist"></userlist-com>
+    </div>                                                      
   </div>
 </template>
 
@@ -39,12 +41,17 @@ export default {
 };
 </script>
 
-<style lang="less">
+<style scoped lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}     
+.content{
+  display: flex;
+  width: 800px;
+  height: 700px;
 }
 </style>
--- Javascript/Vue/vuecli2/src/components/chatcom.vue
+++ Javascript/Vue/vuecli2/src/components/chatcom.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
+  <div class="chat">
     <h1>Chat</h1>
   </div>
 </template>
+
+<style scoped lang="less">
+.chat{
+  background-color: red;blue;        
+  width: w;
+  width: 22px;
+}
+</style>
